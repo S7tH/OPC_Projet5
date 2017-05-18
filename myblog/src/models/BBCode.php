0a2a0cea348302d45339f263561406866c118525
S7tH <aurelien.theriot@gmail.com>
2017-05-18 20:33:10 +0200

    add a target _blank to my bbcode link

--- myblog/src/models/BBCode.php
+++ myblog/src/models/BBCode.php
@@ -12,9 +12,9 @@ class BBCode
         //underline
         $content = preg_replace('`\[s\](.+)\[/s\]`isU', '<u>$1</u>', $content);
         //link with http://
-        $content = preg_replace('`\[url\](http://[a-z0-9._,/?!&=#-]+)\[/url\]`i', '<a href="$1">$1</a>', $content);
+        $content = preg_replace('`\[url\](http://[a-z0-9._,/?!&=#-]+)\[/url\]`i', '<a href="$1" target="_blank">$1</a>', $content);
         //link without http://
-        $content = preg_replace('`\[url\]([a-z0-9._/-]+)\[/url\]`isU', '<a href="http://$1">$1</a>', $content);
+        $content = preg_replace('`\[url\]([a-z0-9._/-]+)\[/url\]`isU', '<a href="http://$1" target="_blank">$1</a>', $content);
 
         
         return $content;
